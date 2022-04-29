b7ca50c8eb515b871d8e2860117db2d1a9f35e7a
eclectify Tutorials <102626571+eclectifyTutorials@users.noreply.github.com>
2022-04-29 14:40:35 +0200

    Added all files

---
+++ main.dart
@@ -0,0 +1,113 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const customSwatch = MaterialColor(
+    0xFFFF5252,
+    <int, Color>{
+      50: Color(0xFFFFEBEE),
+      100: Color(0xFFFFCDD2),
+      200: Color(0xFFEF9A9A),
+      300: Color(0xFFE57373),
+      400: Color(0xFFEF5350),
+      500: Color(0xFFFF5252),
+      600: Color(0xFFE53935),
+      700: Color(0xFFD32F2F),
+      800: Color(0xFFC62828),
+      900: Color(0xFFB71C1C),
+    },
+  );
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: customSwatch,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  LocationData? _locationData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Location"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(onPressed: _determinePosition, child: Text('Determine Position')),
+            SizedBox(height: 20.0,),
+            Text(_locationData!=null? 'Long: ${_locationData!.longitude}, Lat: ${_locationData!.latitude}'
+                : '----',),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _determinePosition() async {
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        print("!_serviceEnabled");
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        print("_permissionGranted != PermissionStatus.granted");
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    print(_locationData);
+
+    /// update ui
+    setState(() {});
+
+
+    /// get continuous location updates
+    // location.onLocationChanged.listen((LocationData currentLocation) {
+    //   // Use current location
+    // });
+
+    /// receive location when application is in background
+    // location.enableBackgroundMode(enable: true);
+  }
+
+}
+
